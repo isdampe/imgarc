2f8518d25ead0f9dd542b87c65ae46d95e83ba01
Richard Denton <isdampe@gmail.com>
2018-01-26 14:31:56 +1100

    Adds bitop functions and procedures

--- src/bitops.c
+++ src/bitops.c
@@ -1,11 +1,16 @@
 #include <stdint.h>
 
-void set_bit_8(uint8_t *data, uint8_t bit_n)
+void imgarc_set_bit_8(uint8_t *data, uint8_t bit_n)
 {
 	*data |= 1 << bit_n;
 }
 
-void clear_bit_8(uint8_t *data, uint8_t bit_n)
+void imgarc_clear_bit_8(uint8_t *data, uint8_t bit_n)
 {
 	*data &= ~(1 << bit_n);
+}
+
+uint8_t imgarc_get_bit_8(const uint8_t *data, uint8_t bit_n)
+{
+	return (*data >> bit_n) & 0x01;
 }
--- src/bitops.h
+++ src/bitops.h
@@ -1,4 +1,5 @@
 #include <stdint.h>
 
-void set_bit_8(uint8_t *data, uint8_t bit_n);
-void clear_bit_8(uint8_t *data, uint8_t bit_n);
+void imgarc_set_bit_8(uint8_t *data, uint8_t bit_n);
+void imgarc_clear_bit_8(uint8_t *data, uint8_t bit_n);
+uint8_t imgarc_get_bit_8(const uint8_t *data, uint8_t bit_n);
