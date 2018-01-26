d4a63b56a7b64d1f917a56b98edc5c6ef11a1b17
Richard Denton <isdampe@gmail.com>
2018-01-26 11:59:28 +1100

    Add bitops

---
+++ src/bitops.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+void set_bit_8(uint8_t *data, uint8_t bit_n)
+{
+	*data |= 1 << bit_n;
+}
+
+void clear_bit_8(uint8_t *data, uint8_t bit_n)
+{
+	*data &= ~(1 << bit_n);
+}
---
+++ src/bitops.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+
+void set_bit_8(uint8_t *data, uint8_t bit_n);
+void clear_bit_8(uint8_t *data, uint8_t bit_n);
