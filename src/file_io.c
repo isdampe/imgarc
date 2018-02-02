a80c461cfded00cc43d7628a0e3ddcc11744d6b7
Richard Denton <isdampe@gmail.com>
2018-02-02 13:34:36 +1100

    Ensure that file name is null-terminated

--- src/file_io.c
+++ src/file_io.c
@@ -12,11 +12,11 @@ imgarc_file imgarc_read_file(char *fp)
 	int n = 0;
 
 	fd.size_bytes = 0;
-	
+
 	fh = fopen(fp, "rb");
 	if (! fh)
 		return fd;
-	
+
 	//Get size in bytes.
 	fseek(fh, 0, SEEK_END);
 	fd.size_bytes = ftell(fh);
@@ -27,6 +27,7 @@ imgarc_file imgarc_read_file(char *fp)
 	size_t sl = strlen(bn) +1;
 	fd.name = malloc(sl * sizeof(char));
 	strncpy(fd.name, bn, sl -1);
+	fd.name[sl -1] = '\0';
 
 	//Alocate space.
 	fd.data = malloc(fd.size_bytes * sizeof(uint8_t));
@@ -57,7 +58,7 @@ void imgarc_free_file(imgarc_file *fd)
 bool imgarc_file_verify_checksum(imgarc_file *fd)
 {
 	uint8_t checksum[SHA1_BLOCK_SIZE];
-	
+
 	SHA1_CTX ctx;
 	sha1_init(&ctx);
 	sha1_update(&ctx, fd->data, fd->size_bytes);
