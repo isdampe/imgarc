1e114cb08183b26a669920ee9cb4287f05bd3bee
Richard Denton <isdampe@gmail.com>
2018-07-14 02:31:58 +1000

    ENHANCE: Modify bitops as macro functions and extern inlines

--- src/bitops.h
+++ src/bitops.h
@@ -1,6 +1,14 @@
 #include <stdint.h>
 
-void imgarc_set_bit_8(uint8_t *data, uint8_t bit_n);
-void imgarc_clear_bit_8(uint8_t *data, uint8_t bit_n);
-uint8_t imgarc_get_bit_8(const uint8_t *data, uint8_t bit_n);
-uint32_t imgarc_bytes_to_uint32(uint8_t *bytes);
+#define imgarc_set_bit_8(a, b)	*a |= 1 << b
+#define imgarc_clear_bit_8(a, b) *a &= ~(1 << b)
+#define imgarc_get_bit_8(a, b) ((*a >> b) & 0x01)
+
+extern inline uint32_t imgarc_bytes_to_uint32(uint8_t *bytes)
+{
+	uint32_t result = 0;
+	for (int n=0; n<4; n++)
+		result = (result << 8) + bytes[n];
+	
+	return result;
+}
